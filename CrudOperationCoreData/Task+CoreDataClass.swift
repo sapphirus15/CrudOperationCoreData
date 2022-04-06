01163db294926b5a1bbf7d55cdf208fa4240ccb9
Ryan Yoon <sapphirus15@gmail.com>
2022-04-06 15:33:20 +0900

    subclass로 Manual 로 추가

---
+++ CrudOperationCoreData/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  CrudOperationCoreData
+//
+//  Created by Ryan on 2022/04/06.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ CrudOperationCoreData/Task+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Task+CoreDataProperties.swift
+//  CrudOperationCoreData
+//
+//  Created by Ryan on 2022/04/06.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var id: Int32
+    @NSManaged public var details: String?
+
+}
+
+extension Task : Identifiable {
+
+}
