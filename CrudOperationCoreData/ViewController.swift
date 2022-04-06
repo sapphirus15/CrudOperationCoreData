5c559bde9e4d67898bc27322070d7c6348b83b47
Ryan Yoon <sapphirus15@gmail.com>
2022-04-06 15:17:54 +0900

    간단한 CRUD 함수 구현 및 테스트

--- CrudOperationCoreData/ViewController.swift
+++ CrudOperationCoreData/ViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        addToTask()
+//        addToTask()
+        fetchTaskFromCoreDate()
+//        deleteTaskFromCoreData()
     }
 
+    func addToTask() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let todoEntity = NSEntityDescription.entity(forEntityName: "Task", in: managedObjectContext)!
+        
+        let todo = NSManagedObject(entity: todoEntity, insertInto: managedObjectContext)
+        
+        todo.setValue("First Item", forKey: "name")
+        todo.setValue("First Item Description", forKey: "details")
+        todo.setValue(1, forKey: "id")
+        
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    func fetchTaskFromCoreDate() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
+        
+        do {
+            let tasks = try managedObjectContext.fetch(fetchRequest)
+            
+            for data in tasks {
+                print(data.value(forKey: "details") ?? "No Data found")
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func deleteTaskFromCoreData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
+        
+        do {
+            let tasks = try managedObjectContext.fetch(fetchRequest)
+            
+            for data in tasks {
+                managedObjectContext.delete(data)
+            }
+            
+            do {
+                try  managedObjectContext.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 
 }
 
