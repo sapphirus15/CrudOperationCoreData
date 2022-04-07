ee683a329ebaa006be9c8685af02aad9a40967a0
Ryan Yoon <sapphirus15@gmail.com>
2022-04-07 14:32:06 +0900

    predicate, sort 를 이용한 data fetch

--- CrudOperationCoreData/ViewController.swift
+++ CrudOperationCoreData/ViewController.swift
@@ -17,9 +17,15 @@ class ViewController: UIViewController {
 //        addToTask()
 //        fetchTaskFromCoreDate()
 //        deleteTaskFromCoreData()
-        addTodoTaskWithObjectOrientedWay()
-        fetchTaskFromCoreDataWithObjectOrientedStyle()
+//        addTodoTaskWithObjectOrientedWay()
+//        fetchTaskFromCoreDataWithObjectOrientedStyle()
 //        deleteTaskFromCoreDataWithObjectOrientedWay()
+//        addUser()
+//        addTwoUsers()
+//        fetchUserFromCoreDataWithPredicate()
+        addThreeUsers()
+        fetchUserFromCoreDataWithSortDescriptor()
+        
     }
 
     
@@ -81,6 +87,104 @@ extension ViewController {
                 try  managedObjectContext.save()
             } catch let error as NSError {
                 print("Could not save. \(error), \(error.userInfo)")
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addUser() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let user = User(context: managedObjectContext)
+        user.secondName = "User Second Name"
+        print("User First Name \(String(describing: user.firstName))")
+        user.userId = 123
+        user.firstName = "Text Contain More Than 12 Characters "
+        
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addTwoUsers() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let user = User(context: managedObjectContext)
+        user.secondName = "User One Second name"
+        user.firstName = "ali"
+        
+        let secondUser = User(context: managedObjectContext)
+        secondUser.secondName = "User Second second name"
+        secondUser.firstName = "Not ali"
+        
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func fetchUserFromCoreDataWithPredicate() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let userFetchRequest = NSFetchRequest<User>(entityName: "User")
+        userFetchRequest.predicate = NSPredicate(format: "firstName == %@", "ali")
+        
+        do {
+            let users = try managedObjectContext.fetch(userFetchRequest)
+            print("Users Count \(users.count)")
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addThreeUsers() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let user = User(context: managedObjectContext)
+        user.secondName = "Vijay"
+        user.firstName = "Kumar"
+        
+        let secondUser = User(context: managedObjectContext)
+        secondUser.secondName = "Subhan"
+        secondUser.firstName = "Ali"
+        
+        let thirdUser = User(context: managedObjectContext)
+        thirdUser.secondName = "Akhtar"
+        thirdUser.firstName = "Ali"
+        
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func fetchUserFromCoreDataWithSortDescriptor() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let userFetchRequest = NSFetchRequest<User>(entityName: "User")
+        
+        let sortByFirstName = NSSortDescriptor.init(key: "firstName", ascending: true)
+        let sortBySecondName = NSSortDescriptor.init(key: "secondName", ascending: true)
+        
+        userFetchRequest.sortDescriptors = [sortByFirstName, sortBySecondName]
+        
+        do {
+            let users = try  managedObjectContext.fetch(userFetchRequest)
+            
+            for user in users {
+                print("User First Name \(String(describing: user.firstName ?? "Default value"))")
+                print("User Second Name \(String(describing: user.secondName ?? "Default value"))")
+                print("\n\n\n")
             }
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
